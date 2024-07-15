83fd14f111a51803a71add525a03876d60fea35c
Mike-kholin <warugu.mike20@students.dkut.ac.ke>
2024-07-15 15:27:22 +0300

    chore:added github to the auth options

--- app/api/authOptions.ts
+++ app/api/authOptions.ts
@@ -1,11 +1,16 @@
 import { NextAuthOptions } from "next-auth";
 import google from "next-auth/providers/google";
+import github from "next-auth/providers/github";
 
 const authOptions: NextAuthOptions = {
   providers: [
     google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
 };
